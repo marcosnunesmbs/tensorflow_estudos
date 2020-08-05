86da8698891aa25f719ef462039c68ad843fe8de
Marcos Nunes <marcosnunesmbs@gmail.com>
2020-08-05 10:53:18 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="bg-orange">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Estudos Tensorflow.js
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Projetos
+        </q-item-label>
+        <div v-for="link in essentialLinks" :key="link.title">
+          <q-item clickable v-ripple class="q-py-md" :to="link.link">
+            <q-item-section avatar>
+              <q-icon color="indigo" :name="link.icon" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label><span>{{link.title}}</span></q-item-label>
+              <q-item-label caption>{{link.caption}}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </div>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+const linksData = [
+  {
+    title: 'Dólar',
+    caption: 'Prevent Dólar',
+    icon: 'school',
+    link: '/dollar'
+  }
+]
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ src/pages/dollar.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-page>
+    <div class="container q-pa-md">
+      <div class="row">
+        <div class="col-12">
+
+          <q-form
+            @submit="send"
+            class="q-gutter-md"
+          >
+            <q-input
+              filled
+              v-model="file"
+              float-label="Dólar historic CSV"
+              hint="Dólar historic CSV"
+              type="file"
+            />
+
+            <div class="row justify-end">
+              <span class="">{{progess}}</span>
+            </div>
+
+            <div>
+              <q-btn label="Predicate" type="submit" color="orange"/>
+            </div>
+          </q-form>
+
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      file: '',
+      progess: 'Aguardando Arquivo'
+    }
+  }
+}
+</script>
